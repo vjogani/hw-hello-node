940a16146fa2202aa7db04a27e1de1a7c2993a63
Vinit Jogani <vinitjogani@gmail.com>
2020-08-16 17:28:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var http = require('http');
 var port = process.env.PORT || 8080;        
 var server = http.createServer(function(req, res) {
     res.writeHead(200);
-    res.end('Hello World!!! This changes is done via GitHub using Review Apps');
+    res.end('Hello World!!! This changes is done via GitHub using Review Apps - v2');
 });
 server.listen(port);
